6abe0d04ed286bfa65c4c16ff1ad7f96f05edbfb
Tony Layher <layhertony@gmail.com>
2011-06-05 11:18:40 -0400

    Vivo:  Use Correct button backlight device.
    -Vivo has 2 entries for button backlights, portait and landscape.
     Enabling just portait for now until a fix for both can be found.

--- liblights/lights.c
+++ liblights/lights.c
@@ -41,7 +41,7 @@ char const*const AMBER_LED_FILE = "/sys/class/leds/amber/brightness";
 char const*const GREEN_LED_FILE = "/sys/class/leds/green/brightness";
 char const*const BLUE_LED_FILE = "/sys/class/leds/blue/brightness";
 
-char const*const BUTTON_FILE = "/sys/class/leds/button-backlight/brightness";
+char const*const BUTTON_FILE = "/sys/class/leds/button-backlight-portrait/brightness";
 
 char const*const AMBER_BLINK_FILE = "/sys/class/leds/amber/blink";
 char const*const GREEN_BLINK_FILE = "/sys/class/leds/green/blink";
