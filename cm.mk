792d27552b84d9576a410aec16ce381aa1464a56
Giulio Cervera <giulio.cervera@gmail.com>
2011-12-22 17:05:03 -0500

    Vivo: update fingerprint

--- cm.mk
+++ cm.mk
@@ -8,7 +8,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vivo BUILD_ID=GRI40 BUILD_FINGERPRINT=htc_wwe/htc_vivo/vivo:2.3.3/GRI40/88716:user/release-keys PRIVATE_BUILD_DESC="2.30.405.1 CL88716 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vivo BUILD_ID=GRJ90 BUILD_FINGERPRINT=htc_wwe/htc_vivo/vivo:2.3.5/GRJ90/197817.3:user/release-keys PRIVATE_BUILD_DESC="3.08.405.3 CL197817 release-keys"
 
 PRODUCT_NAME := cm_vivo
 PRODUCT_DEVICE := vivo
