9411aaa5953364d6d6fa8bda1da8948aa0f2f095
Giulio Cervera <giulio.cervera@gmail.com>
2011-12-22 08:41:44 -0500

    Vivo: fix typo in apns file name

--- full_vivo.mk
+++ full_vivo.mk
@@ -22,7 +22,7 @@
 #
 
 # Get the long list of APNs
-PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf
+PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
 
 # Camera
 PRODUCT_PACKAGES := \
