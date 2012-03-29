37f014808bfd6af200fe5e3ee2de456446c59bd2
Giulio Cervera <giulio.cervera@gmail.com>
2012-03-29 15:33:16 +0200

    Vivo: enable mass storage
    Change-Id: Iaba4111f78c2ca010bbdcb7e97d9593a5d8a171e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -75,3 +75,7 @@ BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk1
 BOARD_SDEXT_DEVICE := /dev/block/mmcblk1p2
 BOARD_USES_MMCUTILS := true
 BOARD_HAS_NO_MISC_PARTITION := true
+
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/usb_mass_storage/lun0/file
