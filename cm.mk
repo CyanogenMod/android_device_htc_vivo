3d31e291993cfd2b92f4c566197906a12354e276
Giulio Cervera <giulio.cervera@gmail.com>
2012-03-29 15:31:38 +0200

    Vivo: don't override build_id
    Change-Id: I61436bdc3f2e800a84789f0b733f12bd32623d45

--- cm.mk
+++ cm.mk
@@ -8,7 +8,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vivo BUILD_ID=GRJ90 BUILD_FINGERPRINT=htc_wwe/htc_vivo/vivo:2.3.5/GRJ90/197817.3:user/release-keys PRIVATE_BUILD_DESC="3.08.405.3 CL197817 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vivo BUILD_FINGERPRINT=htc_wwe/htc_vivo/vivo:2.3.5/GRJ90/197817.3:user/release-keys PRIVATE_BUILD_DESC="3.08.405.3 CL197817 release-keys"
 
 PRODUCT_NAME := cm_vivo
 PRODUCT_DEVICE := vivo
